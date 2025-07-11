0b48620a934ccea51312345bbe07a8b0f32331e5
viveksonawale <sonawalevivek777@gmail.com>
2025-07-11 10:26:37 +0530

    Added projects

--- ChatApplication/src/main/java/org/example/chatapplication/HelloApplication.java
+++ ChatApplication/src/main/java/org/example/chatapplication/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 450, 700);
-        stage.setTitle("Client");
+        Scene scene = new Scene(fxmlLoader.load(), 450, 600);
+        stage.setTitle("Server");
         stage.setScene(scene);
         stage.show();
     }
